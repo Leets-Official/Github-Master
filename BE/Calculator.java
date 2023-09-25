da3f61fa980b7c151b11e5ff0941b4e46132604f
oypG <l2007889@gachon.ac.kr>
2023-09-25 18:34:22 +0900

    plz d

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -5,10 +5,6 @@ public class Calculator {
         int minus = minus(num1, num2);
         System.out.println("뺄셈: " + minus);
     }
-
-    private static int add(int a, int b) {
-        return a + b;
-    }
     private static int minus(int a, int b) {
         return a - b;
     }
