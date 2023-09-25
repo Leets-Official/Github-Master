4cf99cf1f9be9cce5850ea2cf51f7685b5a9fa68
ay-eonii <naayen@naver.com>
2023-09-25 17:49:49 +0900

    feat: minus 함수 추가

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
         int num1 = 5;
         int num2 = 3;
         int result = add(num1, num2);
-        int result2 = subtract(num1, num2);
+        int minus = minus(num1, num2);
         System.out.println("합계: " + result);
         System.out.println("뺄셈: " + minus);
     }
@@ -12,7 +12,8 @@ public class Calculator {
         return a + b;
     }
 
-    private static int subtract(int a, int b) {
+    private static int minus(int a, int b) {
         return a - b;
     }
+
 }
