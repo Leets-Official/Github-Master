1bf4916e59f169ed4fc4602cd5b20db786c5daeb
ggome1 <bookincar@naver.com>
2023-09-25 18:27:59 +0900

    fix

--- FE/Calculator.js
+++ FE/Calculator.js
@@ -24,4 +24,4 @@ const operateMaker = (operate) => (a, b) => operate(a, b);
 const adder = operateMaker(Calculator.add);
 const subtractor = operateMaker(Calculator.substraction);
 const multiply = operateMaker(Calculator.multiplicatoin);
-const divider = operateMaker(Calculator.divide);
+
