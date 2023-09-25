3a48eeb972e66f238b805cc121752adb3400ea1d
taeseokyang <ts.yang.0123@gmail.com>
2023-09-25 18:27:30 +0900

    fix: 컨플릭스 오류를 위한 수정

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
         int result = add(num1, num2);
         int result2 = subtract(num1, num2);
         System.out.println("합계: " + result);
+        System.out.println("뺄셈: " + minus);
     }
 
     private static int add(int a, int b) {
@@ -14,5 +15,4 @@ public class Calculator {
     private static int subtract(int a, int b) {
         return a - b;
     }
-}
 }
