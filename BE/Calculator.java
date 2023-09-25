c16843d0321d0178581cb7b427de10d6e36320a0
taeseokyang <ts.yang.0123@gmail.com>
2023-09-25 17:49:38 +0900

    feat: 빼기 함수 추가

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -3,10 +3,16 @@ public class Calculator {
         int num1 = 5;
         int num2 = 3;
         int result = add(num1, num2);
+        int result2 = subtract(num1, num2);
         System.out.println("합계: " + result);
     }
 
     private static int add(int a, int b) {
         return a + b;
     }
+
+    private static int subtract(int a, int b) {
+        return a - b;
+    }
+}
 }
