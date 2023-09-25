7412cba8da2862104c6f55d5f048c22bc726aac5
oypG <l2007889@gachon.ac.kr>
2023-09-25 18:43:50 +0900

    fix: 오류 해결

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -3,6 +3,9 @@ public class Calculator {
         int num1 = 5;
         int num2 = 3;
         int minus = minus(num1, num2);
+        int result = add(num1, num2);
+        int minus = minus(num1, num2);
+        System.out.println("합계: " + result);
         System.out.println("뺄셈: " + minus);
     }
     private static int add(int a, int b) {
