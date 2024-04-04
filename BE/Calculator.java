69588a9ec6bcf69fbe746646897938efcf1ed302
unknown <leejh5838@naver.com>
2024-04-04 21:18:17 +0900

    곱셈연산추가

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -4,14 +4,15 @@ public class Calculator {
         int num2 = 3;
         int result = add(num1, num2);
         int minus = minus(num1, num2);
+        int multResult = multiply(num1, num2);
         System.out.println("합계: " + result);
         System.out.println("뺄셈: " + minus);
+        System.out.println("곱하기: " + multResult);
     }
   
     private static int add(int a, int b) {
         return a + b;
     }
-    private static int minus(int a, int b) {
-        return a - b;
-    }
+    private static int minus(int a, int b) {return a - b; }
+    private static int multiply(int a, int b) {return a * b; }
 }
