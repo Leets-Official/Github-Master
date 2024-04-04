0b102ca8a8a9f606e36ffa3d0425dcff01c5324c
jj0526 <wjstlgus1@naver.com>
2024-04-04 20:55:07 +0900

    first commit

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -1,11 +1,14 @@
+
+
 public class Calculator {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 3;
         int result = add(num1, num2);
         int minus = minus(num1, num2);
-        System.out.println("합계: " + result);
-        System.out.println("뺄셈: " + minus);
+        System.out.println("합계 : " + result);
+        System.out.println("뺄셈 : " + minus);
+        System.out.println("곱셈 : " + num1*num2);
     }
   
     private static int add(int a, int b) {
