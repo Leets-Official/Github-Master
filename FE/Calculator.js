361d22ed100613830a214521d41ee017c283dd24
yezzan9 <rubyyy718@gmail.com>
2024-04-04 20:50:22 +0900

    조예진 코드개잘짬ㅋ

--- FE/Calculator.js
+++ FE/Calculator.js
@@ -10,6 +10,10 @@ class Calculator {
   static multiplication(a, b) {
     return a * b;
   }
+
+  static sum() {
+    console.log(1 + 3);
+  }
 }
 
 const operateMaker = (operate) => (a, b) => operate(a, b);
@@ -18,3 +22,5 @@ const adder = operateMaker(Calculator.add);
 const subtractor = operateMaker(Calculator.substraction);
 const multiply = operateMaker(Calculator.multiplicatoin);
 // const divider = operateMaker(Calculator.divide);
+
+console.log(Calculator.sum());
