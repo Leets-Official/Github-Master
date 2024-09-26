527bfed9631edbd3efc2a60f40cfe6feb575c7d2
sihyeon <syeonie0526@gmail.com>
2024-09-26 20:14:16 +0900

    1

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -1,8 +1,8 @@
 public class Calculator {
     public static void main(String[] args) {
-        int foo = 5;
-        int bar = 3;
-        int result = add(foo, bar);
+        int foo = 10;
+        int b = 10;
+        int result = add(foo, b);
 
         System.out.println("합계: " + result);
     }
