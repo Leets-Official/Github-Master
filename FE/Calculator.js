556fd053de5ae4c9bbb722e5087fb3c6d1441006
odukong <ohbin1017@gmail.com>
2024-09-26 19:53:20 +0900

    fix: change variables name
    2

--- FE/Calculator.js
+++ FE/Calculator.js
@@ -1,11 +1,11 @@
 class Calculator {
-    static add(a, b) {
-        return a + b;
-    }
+  static minus(a, b) {
+    return a + b;
+  }
 }
 
 const operateMaker = (operate) => (a, b) => operate(a, b);
 
-const adder = operateMaker(Calculator.add);
+const minuses = operateMaker(Calculator.minus);
 
-console.log(Calculator.add());
+console.log(Calculator.minus());
