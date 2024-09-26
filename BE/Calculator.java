c6d2f6e44c4b703ebe382d80f42e20543ac1c5a6
노정완 <jeongwan.noh@gmail.com>
2024-09-26 16:01:26 +0900

    chore: Initial Settings

--- BE/Calculator.java
+++ BE/Calculator.java
@@ -1,24 +1,13 @@
 public class Calculator {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 3;
-        int result = add(num1, num2);
-        int minus = minus(num1, num2);
+        int foo = 5;
+        int bar = 3;
+        int result = add(foo, bar);
 
-        int multResult = multiply(num1, num2);
         System.out.println("합계: " + result);
-        System.out.println("뺄셈: " + minus);
-        System.out.println("곱셈: " + multResult);
     }
-  
+
     private static int add(int a, int b) {
         return a + b;
-    }
-    private static int minus(int a, int b) {
-        return a - b;
-    }
-
-    private static int multiply(int a, int b) {
-        return a * b;
     }
 }
--- FE/Calculator.js
+++ FE/Calculator.js
@@ -1,27 +1,11 @@
 class Calculator {
-  static add(a, b) {
-    return a + b;
-  }
-
-  static subtraction(a, b) {
-    return a - b;
-  }
-
-  static multiplication(a, b) {
-    return a * b;
-  }
-
-  static sum() {
-    console.log(1 + 3);
-  }
+    static add(a, b) {
+        return a + b;
+    }
 }
 
 const operateMaker = (operate) => (a, b) => operate(a, b);
 
 const adder = operateMaker(Calculator.add);
-const subtractor = operateMaker(Calculator.substraction);
-const multiply = operateMaker(Calculator.multiplicatoin);
 
-console.log(Calculator.sum());
-
-console.log(Calculator.sum());
+console.log(Calculator.add());
